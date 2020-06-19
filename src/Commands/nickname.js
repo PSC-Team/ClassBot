48365335d99c8ffe519612db172b8579121bf89e
LOG1CRS <0204571@up.edu.mx>
2020-06-18 21:36:09 -0500

    Bot greeting finished

--- src/Commands/nickname.js
+++ src/Commands/nickname.js
@@ -10,7 +10,7 @@ module.exports.welcome = function (member, Discord) {
   const messageEmbed = new Discord.MessageEmbed()
     .setColor('#41e6a4')
     .setTitle(
-      `Welcome <@${member.displayName}> to the Course ${serverName} server.`
+      `Welcome ${member.displayName} to the Course ${serverName} server.`
     )
     .setDescription(
       'Please type the command "!nickname" and forward your full name, so that your teacher can recognize you within this server.'
@@ -19,6 +19,7 @@ module.exports.welcome = function (member, Discord) {
       'https://i.pinimg.com/originals/98/d3/a2/98d3a283f98cded8e639957e935bd373.png'
     );
 
+  channel.send(member.toString());
   channel.send(messageEmbed);
 };
 

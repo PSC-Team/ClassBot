451fcdfc2363e596bbd734e6e0d634678501d5bf
Felipelv199 <0204021@up.edu.mx>
2020-06-19 19:11:58 -0600

    Notice function added and started

---
+++ src/Commands/notice.js
@@ -0,0 +1,19 @@
+// crear modulo,
+module.exports.notice = function (message, Discord) {
+  let messageSplitted = message.content.split(' ');
+  if (messageSplitted.length === 1) {
+    message.reply('Add a comment after the command');
+    return;
+  }
+  let noticeMessage = message.content.replace('!notice', '').trim();
+
+  message.channel.send('@everyone');
+  const embed = new Discord.MessageEmbed()
+    .setColor('#FC0316')
+    .setTitle('Notice')
+    .setDescription(noticeMessage)
+    .setThumbnail(
+      'https://as2.ftcdn.net/jpg/03/28/49/57/500_F_328495731_wlzrJ94W3BAERUYZy7tr4VZZ8KmssLee.jpg'
+    );
+  message.channel.send(embed);
+};
